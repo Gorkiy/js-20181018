d4249bbc439dcfe080b09af975b8ea3100114a73
Peter Yakovlev <gorkiy@gmail.com>
2018-11-29 13:44:51 +0300

    Feature: Phone gallery thumb clicks now change cover image

--- scripts/phones/components/phone-viewer.js
+++ scripts/phones/components/phone-viewer.js
@@ -3,7 +3,19 @@ import Component from '../../shared/component.js';
 export default class PhoneViewer extends Component {
   constructor({ element }) {
     super({ element });
+
+    this._element.addEventListener('click', this._onThumbClick.bind(this));
   }
+
+  _onThumbClick(event) {
+    let coverImg = this._element.querySelector('[data-element="phone-cover-image"]');
+    let thumbImg = event.target.closest('[data-element="phone-img-thumb"]');
+
+    if (!thumbImg) return;
+
+    coverImg.setAttribute('src', thumbImg.getAttribute('src'));
+
+  };
 
   showPhone(phone) {
     this._phone = phone;
@@ -16,36 +28,24 @@ export default class PhoneViewer extends Component {
     const { _phone: phone } = this;
 
     this._element.innerHTML = `
-      <img class="phone" src="${phone.images[0]}">
+      <img data-element="phone-cover-image" class="phone" src="${phone.images[0]}">
 
-    <button>Back</button>
-    <button>Add to basket</button>
+      <button>Back</button>
+      <button>Add to basket</button>
 
 
-    <h1>${phone.name}</h1>
+      <h1>${phone.name}</h1>
 
-    <p>${phone.description}</p>
+      <p>${phone.description}</p>
 
-    <ul class="phone-thumbs">
-      <li>
-        <img src="img/phones/motorola-xoom-with-wi-fi.0.jpg">
-      </li>
-      <li>
-        <img src="img/phones/motorola-xoom-with-wi-fi.1.jpg">
-      </li>
-      <li>
-        <img src="img/phones/motorola-xoom-with-wi-fi.2.jpg">
-      </li>
-      <li>
-        <img src="img/phones/motorola-xoom-with-wi-fi.3.jpg">
-      </li>
-      <li>
-        <img src="img/phones/motorola-xoom-with-wi-fi.4.jpg">
-      </li>
-      <li>
-        <img src="img/phones/motorola-xoom-with-wi-fi.5.jpg">
-      </li>
-    </ul>
+      <ul class="phone-thumbs">
+        ${ phone.images.map(image => `
+          <li>
+          <img data-element="phone-img-thumb" src="${ image }">
+        </li>
+      `).join('')}
+
+      </ul>
     `;
   }
 }
